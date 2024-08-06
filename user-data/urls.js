39415645f537a1f0bfb08d7ebb0a9e759009c179
healixloo <healixloo@gmail.com>
2024-08-07 01:22:53 +0200

    update blog

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,14 +1,10 @@
 const githubUsername = "vinaysomawat";
-const mediumUsername = "healixloo";
+const mediumUsername = "healixloox";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 
 export const URLs = {
-    get medium() {
-        return createMediumURL(mediumUsername);
-    },
-    get gitConnected() {
-        return createGitConnectedURL(githubUsername);
-    }
+    medium: createMediumURL(mediumUsername),
+    gitConnected: createGitConnectedURL(githubUsername)
 };
