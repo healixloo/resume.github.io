acbcdf93e1e66fa2357424d9545359529bf754b4
healixloo <healixloo@gmail.com>
2024-08-06 19:22:13 +0200

    v0.2

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,7 +1,7 @@
 const githubUsername = "vinaysomawat";
-const mediumUsername = "healixloo";
+const mediumUsername = "vinaysomawat";
 
-const createMediumURL = (username) => `https://${username}.github.io`;
+const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 
 export const URLs = {
